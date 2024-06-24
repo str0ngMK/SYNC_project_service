f0777e123c446fa69bb5ba2fb65a78fc67c051d3
hwlee9905 <9905hyun@naver.com>
2024-06-24 18:28:11 +0900

    [Test]

--- src/main/java/sync/example/TestController.java
+++ src/main/java/sync/example/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/project/api/v1")
 @RequiredArgsConstructor
 public class TestController {
 
