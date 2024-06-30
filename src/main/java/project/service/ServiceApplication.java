6f87eb3b1f395c96d9fa92feee2bcb9795f81d4a
hwlee9905 <9905hyun@naver.com>
2024-07-01 05:11:59 +0900

    [Test]

--- src/main/java/project/service/ServiceApplication.java
+++ src/main/java/project/service/ServiceApplication.java
@@ -1,13 +1,13 @@
-package sync.example;
+package project.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExampleApplication {
+public class ServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleApplication.class, args);
+		SpringApplication.run(ServiceApplication.class, args);
 	}
 
 }
