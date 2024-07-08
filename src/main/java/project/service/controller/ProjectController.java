5a1a63e0a146d6083411c75ed772f0db7528b5b6
hwlee9905 <9905hyun@naver.com>
2024-07-08 09:21:17 +0900

    [Test]

--- src/main/java/project/service/controller/ProjectController.java
+++ src/main/java/project/service/controller/ProjectController.java
@@ -3,6 +3,7 @@ package project.service.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.service.ProjectService;
@@ -14,7 +15,7 @@ import project.service.global.ResponseMessage;
 @Slf4j
 public class ProjectController {
     final ProjectService projectService;
-    @GetMapping("/find")
+    @PostMapping("/find")
     public ResponseMessage findProject(Long projectId)  {
         return projectService.findProject(projectId);
     }
