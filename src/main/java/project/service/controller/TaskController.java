bd7efe98148a17f6cc9cf57745e0da1cb4a6089d
hwlee9905 <9905hyun@naver.com>
2024-07-24 01:01:48 +0900

    [Fix]
    getusersFromTask Dto 수정

--- src/main/java/project/service/controller/TaskController.java
+++ src/main/java/project/service/controller/TaskController.java
@@ -3,6 +3,7 @@ package project.service.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import project.service.TaskService;
 import project.service.dto.request.GetMemberFromTaskRequestDto;
@@ -18,7 +19,7 @@ public class TaskController {
         return taskService.getOnlyChildrenTasks(taskId);
     }
     @GetMapping("/project/task/api/v1/users")
-    public ResponseMessage getUserFromTask(GetMemberFromTaskRequestDto getMemberFromTaskRequestDto)  {
+    public ResponseMessage getUserFromTask(@RequestBody GetMemberFromTaskRequestDto getMemberFromTaskRequestDto)  {
         return taskService.getUserIdsFromTask(getMemberFromTaskRequestDto.getTaskId());
     }
 
